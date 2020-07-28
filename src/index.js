10fe4f3f7252b49505dd8837f096b5966295d227
xiong35 <2242787668@qq.com>
2020-07-28 18:54:07 +0800

    done with btn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { diff, patch } from "./vDom";
+
 class MyVue {
   constructor(options = {}) {
     this.$el = document.querySelector(options.el);
@@ -9,7 +11,12 @@ class MyVue {
     this.methods = options.methods; // 事件方法
     this.observeTasks = {}; // 需要监听的任务列表
     this.observe(data); // 初始化劫持监听所有数据
-    this.compile(this.$el); // 解析dom
+    this.vDom = {
+      tag: null,
+      props: {},
+      children: [],
+    };
+    this.compile(this.$el, this.vDom); // 解析dom
   }
 
   observe = (data) => {
@@ -25,7 +32,6 @@ class MyVue {
         set: (newValue) => {
           if (newValue !== value) {
             value = newValue;
-            console.log(this);
             this.observeTasks[key].forEach((task) => {
               task.notify();
             });
@@ -35,80 +41,97 @@ class MyVue {
     });
   };
 
-  compile = (el) => {
+  compile = (el, Vel) => {
     var nodes = el.childNodes;
+    Vel.tag = el.tagName;
 
     nodes.forEach((node) => {
+      let vNode = {
+        tag: node.tagName,
+        props: {},
+        children: [],
+      };
+      // Vel.children.push(vNode);
       // text nodes
       if (node.nodeType === 3) {
         var text = node.textContent.trim();
-        if (!text) return;
-        this.compileText(node, "textContent");
+        if (!text) node.parentNode.removeChild(node);
+        this.compileText(Vel, "text", text);
+        vNode.tag = "TEXT";
       }
       // element node
       else if (node.nodeType === 1) {
+        // Vel.children.push(vNode);
+
+        let attrs = node.getAttributeNames();
+
+        attrs.forEach((attr) => {
+          if (
+            attr === "v-model" &&
+            (node.tagName === "INPUT" ||
+              node.tagName === "TEXTAREA")
+          ) {
+            let attrVal = node.getAttribute("v-model");
+            this.observeTasks[attrVal].push(
+              new Watcher(vNode, this, attrVal, "value")
+            );
+            node.addEventListener("input", () => {
+              this.data[attrVal] = node.value;
+            });
+            vNode.props.value = node.value;
+          } else if (attr === "v-html") {
+            let attrVal = node.getAttribute("v-html");
+            this.observeTasks[attrVal].push(
+              new Watcher(vNode, this, attrVal, "innerHTML")
+            );
+            vNode.props.innerHTML = this.data[attrVal];
+          } else if (attr.startsWith("v-on:")) {
+            let event = attr.split(":")[1];
+            let attrVal = node.getAttribute(attr);
+
+            node.addEventListener(event, (e) => {
+              this.methods[attrVal] &&
+                this.methods[attrVal].call(this, e);
+            });
+          } else {
+            vNode.props[attr] = node.getAttribute(attr);
+            return;
+          }
+          node.removeAttribute(attr);
+        });
+
         if (node.childNodes.length > 0) {
-          this.compile(node);
-        }
-
-        if (
-          node.hasAttribute("v-model") &&
-          (node.tagName === "INPUT" || node.tagName === "TEXTAREA")
-        ) {
-          let attrVal = node.getAttribute("v-model");
-          this.observeTasks[attrVal].push(
-            new Watcher(node, this, attrVal, "value")
-          );
-          node.removeAttribute("v-model");
-          node.addEventListener("input", () => {
-            this.data[attrVal] = node.value;
-          });
-        }
-
-        if (node.hasAttribute("v-html")) {
-          let attrVal = node.getAttribute("v-html");
-          this.observeTasks[attrVal].push(
-            new Watcher(node, this, attrVal, "innerHTML")
-          );
-          node.removeAttribute("v-html");
-        }
-
-        this.compileText(node, "innerHTML");
-
-        if (node.hasAttribute("v-on:click")) {
-          let attrVal = node.getAttribute("v-on:click");
-
-          node.removeAttribute("v-on:click");
-          node.addEventListener("click", (e) => {
-            this.methods[attrVal] &&
-              this.methods[attrVal].call(this);
-          });
+          this.compile(node, vNode);
         }
       }
     });
   };
 
-  compileText = (node, type) => {
-    let reg = /\{\{(.*?)\}\}/g,
-      txt = node.textContent;
-    if (reg.test(txt)) {
-      node.textContent = txt.replace(reg, (matched, group1) => {
-        let code = group1.trim();
+  compileText = (vNode, type, text) => {
+    let reg = /\{\{(.*?)\}\}/g;
+    if (reg.test(text)) {
+      vNode.children.push(
+        text.replace(reg, (matched, group1) => {
+          let code = group1.trim();
 
-        let tasks = this.observeTasks[code] || [];
-        tasks.push(new Watcher(node, this, code, type));
+          let tasks = this.observeTasks[code] || [];
+          tasks.push(new Watcher(vNode, this, code, type));
 
-        if (code.split(".").length > 1) {
-          // 如果是形如 foo.bar 的代码, 就逐层找下去
-          let v = null;
-          code.split(".").forEach((val, i) => {
-            v = v ? v[val] : this[val];
-          });
-          return v;
-        } else {
-          return this[code];
-        }
-      });
+          if (code.split(".").length > 1) {
+            // 如果是形如 foo.bar 的代码, 就逐层找下去
+            let v = null;
+            // foo[0]无法处理
+            code.split(".").forEach((val) => {
+              v = v ? v[val] : this[val];
+            });
+            return v;
+          } else {
+            return this[code];
+          }
+        })
+      );
+    } else {
+      vNode.children.push(text);
     }
   };
 }
@@ -126,30 +149,59 @@ function proxy(target, sourceKey, key) {
   });
 }
 
+var timeout = null;
+
 class Watcher {
-  constructor(el, vm, value, type) {
-    this.el = el;
+  constructor(vNode, vm, code, type) {
+    this.vNode = vNode;
     this.vm = vm;
-    this.value = value;
+    this.code = code;
     this.type = type;
-    this.notify();
+    // this.notify();
   }
 
   notify = () => {
-    this.el[this.type] = this.vm.data[this.value];
+    if (timeout) {
+      return;
+    }
+    timeout = setTimeout(() => {
+      timeout = null;
+    }, 20);
+    // debugger;
+
+    let oldTree = JSON.parse(JSON.stringify(this.vm.vDom));
+    let v = null;
+    // foo[0]无法处理
+    this.code.split(".").forEach((val) => {
+      v = v ? v[val] : this.vm[val];
+    });
+    if (this.type === "text") {
+      this.vNode.children[0] = v;
+    } else if (this.type === "value") {
+      this.vNode.props.value = v;
+    } else if (this.type === "innerHTML") {
+      this.vNode.props.innerHTML = v;
+    }
+    console.log("vm:", this.vm);
+    let patchObj = diff(oldTree, this.vm.vDom);
+    console.log("patchObj:", patchObj);
+    debugger;
+    // patch(this.vm.$el, patchObj);
+    patch(document.getElementById("parent"), patchObj);
   };
 }
 
-const vm = new MyVue({
+var vm = new MyVue({
   el: "#app",
   data: {
     form: 0,
     test: "<strong>我是粗体</strong>",
   },
   methods: {
-    increment() {
-      console.log(this.form);
+    increment(e) {
       this.form = this.form * 1 + 1;
     },
   },
 });
+
+console.log(vm);
---
+++ src/vDom.js
@@ -0,0 +1,343 @@
+const nodePatchTypes = {
+  CREATE: "create node",
+  REMOVE: "remove node",
+  REPLACE: "replace node",
+  UPDATE: "update node",
+  INSERT: "insert node",
+};
+
+const propPatchTypes = {
+  REMOVE: "remove prop",
+  UPDATE: "update prop",
+};
+
+var createElement = (vDom) => {
+  if (typeof vDom === "string" || typeof vDom === "number") {
+    return document.createTextNode(vDom);
+  }
+
+  const { tag, props, children } = vDom;
+
+  const element = document.createElement(tag);
+
+  for (let propName in props) {
+    element.setAttribute(propName, props[propName]);
+  }
+
+  if (children) {
+    children
+      .map(createElement)
+      .forEach(element.appendChild.bind(element));
+  }
+  return element;
+};
+
+var toVDom = (tag, props, ...children) => {
+  return {
+    tag,
+    props: props || {},
+    children: [].concat.apply([], children) || [],
+  };
+};
+
+export var diff = (vDomOld, vDomNew) => {
+  // debugger;
+  // add
+  if (!vDomOld) {
+    return {
+      type: nodePatchTypes.CREATE,
+      vDom: vDomNew,
+    };
+  }
+
+  // del
+  if (!vDomNew) {
+    return { type: nodePatchTypes.REMOVE };
+  }
+
+  // replace
+  if (
+    typeof vDomOld !== typeof vDomNew ||
+    ((typeof vDomOld === "string" ||
+      typeof vDomOld === "number") &&
+      vDomOld !== vDomNew) ||
+    vDomOld.tag !== vDomNew.tag
+  ) {
+    return {
+      type: nodePatchTypes.REPLACE,
+      vDom: vDomNew,
+    };
+  }
+
+  // update child
+  if (vDomOld.tag) {
+    const propsDiff = _diffProps(vDomOld, vDomNew);
+    const { children, moves } = _diffChildren(vDomOld, vDomNew);
+
+    if (
+      propsDiff.length > 0 ||
+      children.some((patch) => !!patch)
+    ) {
+      return {
+        type: nodePatchTypes.UPDATE,
+        props: propsDiff,
+        moves,
+        children,
+      };
+    }
+  }
+};
+
+var _diffProps = (vDomOld, vDomNew) => {
+  const patches = [];
+  const allProps = { ...vDomOld.props, ...vDomNew.props };
+
+  for (let propName in allProps) {
+    const oldVal = vDomOld.props[propName];
+    const newVal = vDomNew.props[propName];
+
+    // del
+    if (!newVal) {
+      patches.push({
+        type: propPatchTypes.REMOVE,
+      });
+    }
+    // update
+    else if (!oldVal || oldVal !== newVal) {
+      patches.push({
+        type: propPatchTypes.UPDATE,
+        propName,
+        value: newVal,
+      });
+    }
+  }
+  return patches;
+};
+
+var _diffChildren = (vDomOld, vDomNew) => {
+  const patches = new Array(vDomNew.children.length);
+  const moves = new Array(vDomOld.children.length);
+  /* 
+    node obj:
+    {
+      oldInd: <number>
+      vDom: <vDom>
+    }
+    */
+
+  const nodesWithKey = {};
+
+  const nodesWithoutKey = [];
+  let noKeyInd = 0;
+
+  const oldChildren = vDomOld.children;
+
+  const newChildren = vDomNew.children;
+
+  let lastIndex = 0;
+
+  // 将子元素分成有key和没key两组
+  oldChildren.forEach((child, ind) => {
+    const props = child.props;
+
+    if (props !== undefined && props.key !== undefined) {
+      nodesWithKey[props.key] = {
+        oldInd: ind,
+        vDom: child,
+      };
+    } else {
+      nodesWithoutKey[noKeyInd++] = {
+        oldInd: ind,
+        vDom: child,
+      };
+    }
+  });
+
+  // 插入 / 更新新元素, 移动老元素
+  newChildren.forEach((newChild, newInd) => {
+    // 找能不能复用
+    let oldObj;
+    if (newChild && newChild.props && newChild.props.key) {
+      oldObj = nodesWithKey[newChild.props.key];
+      // 设置 key 被用过了
+      nodesWithKey[newChild.props.key] = undefined;
+    } else {
+      // 尝试找到没有 key 的相同元素, 找到就复用, 找不到就插入
+      oldObj = nodesWithoutKey.find((obj, ind) => {
+        if (obj && _sameVnodeType(obj.vDom, newChild)) {
+          nodesWithoutKey[ind] = undefined;
+          return true;
+        }
+      });
+    }
+    // 移动 / 插入 元素
+    if (!oldObj) {
+      // 如果对应的 key 没有旧元素可复用
+      // 就在新元素的位置创建并 插入 元素
+      patches[newInd] = {
+        insert: true,
+        rDom: createElement(newChild),
+        at: newInd,
+      };
+    } else {
+      if (oldObj.oldInd < lastIndex) {
+        /* 当前节点在老集合中的位置与 lastIndex 进行比较，
+           if (child.oldInd >= lastIndex)，
+           说明老元素中, 他前面的节点都被处理过 / 将要被处理
+           他自己只要顺位往前排就好了, 不会有人插到前面
+           则不进行节点移动操作，否则执行该操作 */
+        moves[oldObj.oldInd] = {
+          type: nodePatchTypes.INSERT,
+          from: oldObj.oldInd,
+          at: newInd,
+        };
+      }
+      lastIndex = Math.max(oldObj.oldInd, lastIndex);
+      // 移动后如何更新新dom
+      patches[newInd] = diff(oldObj.vDom, newChild);
+    }
+  });
+
+  // 清理未被复用的元素
+  for (let key in nodesWithKey) {
+    let oldObj = nodesWithKey[key];
+    if (oldObj) {
+      moves[oldObj.oldInd] = {
+        type: nodePatchTypes.REMOVE,
+        from: oldObj.oldInd,
+      };
+    }
+  }
+  for (let ind = 0; ind < nodesWithKey.length; ind++) {
+    let oldObj = nodesWithKey[ind];
+    if (oldObj) {
+      moves[oldObj.oldInd] = {
+        type: nodePatchTypes.REMOVE,
+        from: oldObj.oldInd,
+      };
+    }
+  }
+
+  return { children: patches, moves };
+};
+
+export var patch = (parent, patchObj, index = 0) => {
+  if (!patchObj || !parent) {
+    return;
+  }
+
+  // insert
+  if (patchObj.insert) {
+    let atNode = parent.childNodes[patchObj.at];
+    parent.insertBefore(patchObj.rDom, atNode);
+  }
+
+  // create
+  else if (patchObj.type === nodePatchTypes.CREATE) {
+    return parent.appendChild(createElement(patchObj.vDom));
+  }
+
+  const siblings = parent.childNodes;
+  const element = siblings[index];
+
+  // delete
+  if (patchObj.type === nodePatchTypes.REMOVE) {
+    return parent.removeChild(element);
+  }
+  // replace
+  else if (patchObj.type === nodePatchTypes.REPLACE) {
+    return parent.replaceChild(
+      createElement(patchObj.vDom),
+      element
+    );
+  }
+
+  // update
+  else if (patchObj.type === nodePatchTypes.UPDATE) {
+    const { props, children, moves } = patchObj;
+    _patchProps(element, props);
+
+    // move
+    if (moves) {
+      const toRemove = [];
+
+      moves.forEach((obj) => {
+        if (obj) {
+          if (obj.type === nodePatchTypes.INSERT) {
+            children[obj.to] = children[obj.to] || {};
+
+            children[obj.to].rDom = siblings[obj.from];
+            children[obj.to].insert = true;
+            children[obj.to].at = obj.at;
+          }
+          toRemove.push(siblings[obj.from]);
+        }
+      });
+
+      toRemove.forEach((el) => {
+        if (el) el.parentNode.removeChild(el);
+      });
+    }
+
+    children.forEach((obj, ind) => {
+      patch(element, obj, ind);
+    });
+  }
+};
+
+var _patchProps = (element, props) => {
+  if (!props) {
+    return;
+  }
+
+  props.forEach((obj) => {
+    // del
+    if (obj.type === propPatchTypes.REMOVE) {
+      element.removeAttribute(patchObj.propName);
+    }
+    // create / update
+    else if (obj.type === propPatchTypes.UPDATE) {
+      if (obj.propName === "innerHTML") {
+        element.innerHTML = obj.value;
+      } else {
+        element.setAttribute(obj.propName, obj.value);
+      }
+    }
+  });
+};
+
+var render = (parent, vDom) => {
+  let i = 1;
+  let preDom = getDom(1),
+    newDom;
+
+  const el = createElement(preDom);
+  parent.appendChild(el);
+
+  let timeout = setInterval(() => {
+    if (i > 15) {
+      clearInterval(timeout);
+    }
+    newDom = getDom(++i);
+    const patch = diff(preDom, newDom);
+    console.log(patch);
+    preDom = JSON.parse(JSON.stringify(newDom));
+
+    patch(parent, patch);
+  }, 1000);
+};
+
+var _sameVnodeType = (a, b) => {
+  let aType = typeof a;
+  let bType = typeof b;
+  if (aType === "undefined" || bType === "undefined") {
+    return false;
+  }
+  if (
+    (aType === "string" || aType === "number") &&
+    (bType === "string" || bType === "number")
+  ) {
+    return true;
+  }
+  return a.key === b.key && a.tag === b.tag;
+};
