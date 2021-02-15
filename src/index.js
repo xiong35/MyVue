547208672c270b77552d2eefe30a4d09123e1b97
xiong35 <2242787668@qq.com>
2021-02-15 16:14:54 +0800

    add render and diff

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ function h(tag, props, children) {
 }
 
 function mount(vNode, container) {
-  const el = document.createElement(vNode.tag);
+  const el = (vNode.el = document.createElement(vNode.tag));
 
   // props
   if (vNode.props) {
@@ -31,8 +31,80 @@ function mount(vNode, container) {
   container.appendChild(el);
 }
 
+function patch(n1, n2) {
+  const el = n1.el;
+
+  if (n1.tag === n2.tag) {
+    // props
+    const oldProps = n1.props || {};
+    const newProps = n2.props || {};
+    for (const key in newProps) {
+      const oldVal = oldProps[key];
+      const newVal = newProps[key];
+      if (newVal !== oldVal) {
+        el.setAttribute(key, newVal);
+      }
+    }
+    for (const key in oldProps) {
+      if (!(key in newProps)) {
+        el.removeAttribute(key);
+      }
+    }
+
+    // children
+    const oldChildren = n1.children;
+    const newChildren = n2.children;
+    if (typeof newChildren === "string") {
+      if (typeof oldChildren === "string") {
+        if (newChildren !== oldChildren) {
+          el.textContent = newChildren;
+        }
+      } else {
+        el.textContent = newChildren;
+      }
+    } else {
+      if (typeof oldChildren === "string") {
+        el.innerHTML = "";
+        newChildren.forEach((child) => {
+          mount(child, el);
+        });
+      } else {
+        // TODO key?
+        const commonLen = Math.min(
+          oldChildren.length,
+          newChildren.length
+        );
+        for (let i = 0; i < commonLen; i++) {
+          patch(oldChildren[i], newChildren[i]);
+        }
+        if (newChildren.length > oldChildren.length) {
+          newChildren
+            .slice(oldChildren.length)
+            .forEach((child) => {
+              mount(child, el);
+            });
+        } else {
+          oldChildren
+            .slice(newChildren.length)
+            .forEach((child) => {
+              el.removeChild(child.el);
+            });
+        }
+      }
+    }
+  } else {
+    const parent = el.parentNode;
+    mount(n2, parent);
+    parent.removeChild(el);
+  }
+}
+
 const vNode = h("div", { class: "red" }, [
   h("span", null, "hello"),
 ]);
 
+const vNode2 = h("div", { class: "green" }, [h("p", null, "wow")]);
+
 mount(vNode, document.getElementById("app"));
+
+patch(vNode, vNode2);
